d0c04b57ae8d67204ba3926d133cc8e50bbca224
Jay Lin <cooloojayoo@gmail.com>
2022-01-16 16:05:36 +1100

    Update - Background color

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,7 +4,7 @@ const MovieCard = ({image, title, description}) => {
     const formatText = description.replace("<p>", "").replace("</p>", "").replace("<b>", "").replace("</b>", "")
     if (image === null) return <></>
     return (
-        <div className="w-96 h-full mx-auto bg-purple-300 shadow-lg rounded-xl">
+        <div className="w-96 h-full mx-auto shadow-lg rounded-xl">
             <img src={image.original} alt={title}/>
             <div className="p-4">
                 <h1 className="text-left">{title}</h1>
